ea14efded9e22f531f4e7714a781b8889f6aac1b
Olaf van der Kaaij <omkaaij@gmail.com>
2020-10-22 13:54:21 +0200

    Coordinator aangemaakt

---
+++ src/main/java/model/Coordinator.java
@@ -0,0 +1,16 @@
+package model;
+
+/*
+gemaakt door Olaf
+ */
+
+public class Coordinator extends User {
+
+    public Coordinator (String rolNaam, String naam, String wachtwoord) {
+        super("coordinator", naam, wachtwoord);
+    }
+
+
+
+}
+
