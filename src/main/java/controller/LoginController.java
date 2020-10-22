1b2fb0f4177a51a9cd18def3195d1ce2a04720b3
Olaf van der Kaaij <omkaaij@gmail.com>
2020-10-22 18:34:06 +0200

    login scherm work in progress

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,18 +1,24 @@
 package controller;
-
+import database.mysql.DBAccess;
+import database.mysql.UserDAO;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-
+import model.User;
+import view.Main;
 public class LoginController {
-
+    private UserDAO userDAO;
+    private DBAccess dBaccess;
+    public LoginController() {
+        this.dBaccess = Main.getDBaccess();
+        this.userDAO = new UserDAO(dBaccess);
+    }
     @FXML
     private TextField nameTextField;
     @FXML
     private TextField passwordField;
-
     public void doLogin() {
-
+        User user = userDAO.getOneByName(nameTextField.getText());
+        //todo: checken of gebruikersnaam wel bestaat en wachtwoord goed is
     }
-
     public void doQuit() {}
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,3 +1,7 @@
 package model;
 
-public class User {}
+public class User {
+
+
+
+}
