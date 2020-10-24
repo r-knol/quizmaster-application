f3aaffed25899f94693dae38e60bdde7e0e1a24c
Olaf van der Kaaij <omkaaij@gmail.com>
2020-10-24 19:05:53 +0200

    WelcomeController done met switch

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -28,7 +28,47 @@ public class WelcomeController {
         welcomeLabel.setText("Welkom " + Main.getUser().getNaam() + " , je bent nu ingelogd als " + Main.getUser().getRolNaam());
 
         // Per ingelogde gebruiker krijg je een welkomscherm die hoort bij de rol van de gebruiker.
-        if (Main.getUser().getRolNaam().equals("student")) {
+        String role = Main.getUser().getRolNaam();
+        switch (role) {
+
+            case "student" :
+
+                MenuItem item1 = new MenuItem("In- en uitschrijven cursus.");
+                item1.setOnAction(event -> Main.getSceneManager().showStudentSignInOutScene());
+                taskMenuButton.getItems().add(item1);
+
+                MenuItem item2 = new MenuItem("Quiz selecteren.");
+                item2.setOnAction(event -> Main.getSceneManager().showSelectQuizForStudent());
+                taskMenuButton.getItems().add(item2);
+
+                MenuItem item3 = new MenuItem("Quiz invullen.");
+                item3.setOnAction(event -> Main.getSceneManager().showFillOutQuiz(quiz));
+                taskMenuButton.getItems().add(item3);
+                break;
+
+            case "coordinator" :
+
+                MenuItem item4 = new MenuItem("Ga door naar Dashboard.");
+                item4.setOnAction(event -> Main.getSceneManager().showCoordinatorDashboard());
+                taskMenuButton.getItems().add(item4);
+                break;
+
+            case "administrator" :
+
+                MenuItem item5 = new MenuItem("Ga door naar Cursusbeheer");
+                item5.setOnAction(event -> Main.getSceneManager().showManageCoursesScene());
+                taskMenuButton.getItems().add(item5);
+                break;
+
+            case "technisch beheerder" :
+
+                MenuItem item6 = new MenuItem("Ga naar Gebruikersbeheer.");
+                item6.setOnAction(event -> Main.getSceneManager().showManageUserScene());
+                taskMenuButton.getItems().add(item6);
+                break;
+        }
+
+        /*if (Main.getUser().getRolNaam().equals("student")) {
             MenuItem item1 = new MenuItem("In- en uitschrijven cursus.");
             item1.setOnAction(event -> Main.getSceneManager().showStudentSignInOutScene());
             taskMenuButton.getItems().add(item1);
@@ -50,7 +90,7 @@ public class WelcomeController {
             MenuItem item = new MenuItem("Ga naar Gebruikersbeheer.");
             item.setOnAction(event -> Main.getSceneManager().showManageUserScene());
             taskMenuButton.getItems().add(item);
-        }
+        }*/
     }
     // uitloggen
     public void doLogout(ActionEvent event) {
