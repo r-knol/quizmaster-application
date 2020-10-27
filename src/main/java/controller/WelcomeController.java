6164c7613fd9500b7ba809cf46c0800ae6b1eb6d
Wendy Ellens <wendyellens@gmail.com>
2020-10-27 21:32:18 +0100

    User & UserDAO aangepast

--- src/main/java/controller/WelcomeController.java
+++ src/main/java/controller/WelcomeController.java
@@ -1,19 +1,15 @@
 package controller;
 import database.mysql.DBAccess;
-import database.mysql.UserDAO;
-import javafx.application.Application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import model.Question;
-import model.Quiz;
 import view.Main;
 
-/*
-    @author Olaf van der Kaaij
+/**
+ * @author Olaf van der Kaaij
  */
 
 public class WelcomeController {
@@ -30,12 +26,12 @@ public class WelcomeController {
     private MenuButton taskMenuButton;
 
     public void setup() {
-        // De volgende regel past de tekst uit de view (fxml) aan gebruiker die is ingelogd aan
+        // Welkomsttekst aanpassen aan de gebruiker die is ingelogd
         welcomeLabel.setText("Welkom " + Main.getUser().getVoornaam() + ", je bent ingelogd als " +
-                Main.getUser().getRolNaam());
+                Main.getUser().getRol());
 
-        // Per ingelogde gebruiker krijg je een welkomscherm die hoort bij de rol van de gebruiker
-        switch (Main.getUser().getRolNaam()) {
+        // Taken tonen die horen bij de rol van de ingelogde gebruiker
+        switch (Main.getUser().getRol()) {
 
             case "student" :
 
