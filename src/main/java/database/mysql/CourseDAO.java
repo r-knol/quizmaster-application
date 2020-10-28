61a4e663734a54c5f037d49f56da842ef7839d5f
Wendy Ellens <wendyellens@gmail.com>
2020-10-28 22:06:11 +0100

    Course en CourseDAO geschreven

--- src/main/java/database/mysql/CourseDAO.java
+++ src/main/java/database/mysql/CourseDAO.java
@@ -1,2 +1,89 @@
-package database.mysql;public class CourseDAO {
+package database.mysql;
+
+import model.Course;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author Wendy Ellens
+ */
+
+public class CourseDAO extends AbstractDAO implements GenericDAO<Course> {
+
+    public CourseDAO(DBAccess dbAccess) {
+        super(dbAccess);
+    }
+
+    @Override
+    public ArrayList getAll() {
+        String sql = "Select * FROM Cursus";
+        ArrayList<Course> result = new ArrayList<>();
+        try {
+            setupPreparedStatement(sql);
+            ResultSet resultSet = executeSelectStatement();
+            Course course;
+            while (resultSet.next()) {
+                int cursusID = resultSet.getInt("cursusID");
+                String cursusNaam = resultSet.getString("cursusNaam");
+                int coordinatorID = resultSet.getInt("coordinatorID");
+                course = new Course(cursusID, cursusNaam, coordinatorID);
+                result.add(course);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL error " + e.getMessage());
+        }
+        return result;
+    }
+
+    @Override
+    public Course getOneById(int cursusID) {
+        String sql = "Select * From Cursus Where cursusID = ?";
+        Course result = null;
+        try {
+            setupPreparedStatement(sql);
+            preparedStatement.setInt(1, cursusID);
+            ResultSet resultSet = executeSelectStatement();
+            if (resultSet.next()) {
+                String cursusNaam = resultSet.getString("cursusNaam");
+                int coordinatorID = resultSet.getInt("coordinatorID");
+                result = new Course(cursusID, cursusNaam, coordinatorID);
+            } else {
+                System.out.println("Cursus met dit cursusnummer bestaat niet");
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+        return result;
+    }
+
+    @Override
+    public void storeOne(Course course) {
+        String sql = "Insert into Cursus(cursusnaam, coordinatorID) values(?,?) ;";
+        try {
+            setupPreparedStatement(sql);
+            preparedStatement.setString(1, course.getCursusNaam());
+            preparedStatement.setInt(2, course.getCoordinatorID());
+            int key = executeInsertStatementWithKey();
+            course.setCursusID(key);
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+    }
+
+    public void updateCustomer(Course course) {
+        String sql = "Update Cursus Set cursusNaam = ?, coordinatorID = ? Where cursusID = ?;";
+        try {
+            setupPreparedStatement(sql);
+            preparedStatement.setString(1, course.getCursusNaam());
+            preparedStatement.setInt(2, course.getCoordinatorID());
+            preparedStatement.setInt(3, course.getCursusID());
+            executeManipulateStatement();
+        } catch (SQLException sqlException) {
+            System.out.println("SQL error " + sqlException.getMessage());
+        }
+    }
+
 }
--- src/main/java/model/Course.java
+++ src/main/java/model/Course.java
@@ -1,3 +1,39 @@
 package model;
 
-public class Course {}
+/**
+ * @author Wendy Ellens
+ */
+
+public class Course {
+
+    private int cursusID;
+    private String cursusNaam;
+    private int coordinatorID;
+
+    public Course(int cursusID, String cursusNaam, int coordinatorID) {
+        this.cursusID = cursusID;
+        this.cursusNaam = cursusNaam;
+        this.coordinatorID = coordinatorID;
+    }
+
+    public Course(String cursusNaam, int coordinatorID) {
+        this.cursusNaam = cursusNaam;
+        this.coordinatorID = coordinatorID;
+    }
+
+    public int getCursusID() {
+        return cursusID;
+    }
+
+    public void setCursusID(int cursusID) {
+        this.cursusID = cursusID;
+    }
+
+    public String getCursusNaam() {
+        return cursusNaam;
+    }
+
+    public int getCoordinatorID() {
+        return coordinatorID;
+    }
+}
