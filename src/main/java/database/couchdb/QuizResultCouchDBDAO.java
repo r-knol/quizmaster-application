4f0d10318566f0868855686279f25b727a171fc6
Richard Knol <richard.knol@live.nl>
2020-11-03 20:22:04 +0100

    couchdb in main gezet en method in DAO gemaakt om te testen; werkt

--- src/main/java/database/couchdb/QuizResultCouchDBDAO.java
+++ src/main/java/database/couchdb/QuizResultCouchDBDAO.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import model.QuizResult;
+import model.User;
 
 public class QuizResultCouchDBDAO {
 
@@ -20,9 +21,19 @@ public class QuizResultCouchDBDAO {
     }
 
     // todo: Json string maken van een eenvoudig object
-//    QuizResult eenResult = new QuizResult(quizID, gebruikerID, pogingnr, resultaat, datum);
-//    Gson gson = new Gson();
-//    String resultaatJson = gson.toJson(eenResult);
+    public void runTest() {
+        User user1 = new User("Student", "Richard", "", "Knol");
+        user1.setWachtwoord("hoi");
+        Gson gson = new Gson();
+        String jsonstring = gson.toJson(user1);
+        String gebruiker = gson.toJson(user1);
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(jsonstring).getAsJsonObject();
+        String doc_iD = db.saveDocument(jsonObject);
+        System.out.println("Info als Json: ");
+        System.out.println(gebruiker);
+        System.out.println(doc_iD);
+    }
 
     // todo: mogelijke oplossing voor opslaan enkele poging in Json object?
 //    public String saveSingleQuizResult(QuizResult quizresult) {
@@ -33,8 +44,5 @@ public class QuizResultCouchDBDAO {
 //        String doc_Id = db.saveDocument(jsonObject);
 //        return doc_Id;
 //    }
-
-
-
 
 }
