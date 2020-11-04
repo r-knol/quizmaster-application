6302cf1c5261f34c7c27b4119cdd057c4c1e39e5
Olaf van der Kaaij <omkaaij@gmail.com>
2020-11-04 10:14:28 +0100

    UnitTest QuizDAOTest toegevoegd

---
+++ src/test/java/database/mysql/QuizDAOTest.java
@@ -0,0 +1,42 @@
+package database.mysql;
+
+/*
+* @author Olaf van der Kaaij
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuizDAOTest {
+
+// private final Quiz actual = new Quiz(1, 1, "Geschiedenis", 20, 10);
+
+    @Test
+    void getAll(){
+    }
+
+    @Test
+    void getAllByCourseId() {
+ //       assertEquals(actual.getQuizID(), 1);
+ //       assertEquals(actual.getCursusNaam(), "Media");
+ //       assertEquals(actual.getQuizID(), 4);
+ //       asserEquals(actual.getCursusNaam(), "Wiskunde");
+    }
+
+    @Test
+    void getOneById(){
+    }
+
+    @Test
+    void storeOne(){
+    }
+
+    @Test
+    void updateOne() {
+    }
+
+    @Test
+    void deleteOne(){
+    }
+}
